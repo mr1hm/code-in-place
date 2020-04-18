b861e06af4dda35276eb92dc053ff409582eccba
Kevin Ihm <kevinihm@gmail.com>
2020-04-18 16:18:29 -0700

    added reverse_direction() helper function

--- CS106A-Stanford/Assignment1/MidpointKarel.py
+++ CS106A-Stanford/Assignment1/MidpointKarel.py
@@ -38,7 +38,7 @@ def place_beepers_on_row():
         put_beeper()
         move()
     reverse_direction()  # when Karel stops, we know he's reached the wall, so we can simply reverse his direction.
-    while front_is_clear():  # same idea as above here except in the opposite direction.
+    while front_is_clear():  # same idea here.
         move()
     if beepers_present():  # while also the same idea, Karel will pick up the beeper that's on the first corner of the wall.
         pick_beeper()
@@ -51,32 +51,59 @@ def remove_outer_most_beepers():
         pick_beeper()
         move()
         while beepers_present():  # Karel will move until he lands on the first corner with no beeper.
+            move()  # In other words, Karel will move until it has reached the corner adjacent to the wall.
+        reverse_direction()  # Face Karel in the opposite direction.
+        while no_beepers_present():  # Karel will continue to move the opposite direction if there are no beepers.
+            move()  # We can safely assume that Karel will move until he arrives at a corner with a beeper (outer-most beeper).
+        pick_beeper()  # Then Karel will simply pick the outer-most beeper up.
+
+
+def check_surrounding():  # This function assumes Karel is now standing on a corner with no beeper.
+    while no_beepers_present():  # Karel will move until it reaches a corner with a beeper.
+        move()  # We can safely assume that this is the outer-most beeper.
+    if beepers_present():  # If a beeper is present, we will hard code a manual check for other beepers.
+        move()
+        if beepers_present():  # If a beeper is present here, this means that there are 2 beepers.
             move()
-            if front_is_blocked():  # Do we need this?
-                reverse_direction()
-        while no_beepers_present():  # Karel will continue to move as long as there are no beepers.
-            move()  # We can safely assume that Karel will move until he is standing on a corner with a beeper.
-        pick_beeper()
+        else:  # If there is no 2nd beeper. Stop Karel, turn him around, and move back one corner to land on the midpoint.
+            reverse_direction()
+            move()
+    else:
+        reverse_direction()
 
 
-def check_surrounding():
-
-
-
-
-# def reverse_direction():
-#     turn_left()
-#     turn_left()
-#
-#
-# def return_to_beeper_position():
-#     turn_left()
-#     turn_left()
-#     while no_beepers_present():
+# def check_surrounding_corners():
+#     if corner_color_is(PINK):
 #         move()
+#     if corner_color_is(ORANGE):
+#         reverse_direction()
+#         move()
+#         move()
+#         if corner_color_is(ORANGE):
+#             reverse_direction()
+#             move()
+#         else:
+#             move()
+
+
+# def move_while_paint_is_gray():
+#     while corner_color_is(GRAY):
+#         move()
+
+
+def reverse_direction():
+    turn_left()
+    turn_left()
+
+
+def return_to_beeper_position():
+    turn_left()
+    turn_left()
+    while no_beepers_present():
+        move()
 
 
 # There is no need to edit code beyond this point
 
-    if __name__ == "__main__":
-        run_karel_program()
+if __name__ == "__main__":
+    run_karel_program()
