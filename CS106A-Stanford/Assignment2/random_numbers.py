f1c516cf2a9a4922e881d0c5d722f44900864f42
Kevin Ihm <kevinihm@gmail.com>
2020-04-23 14:41:54 -0700

    completeled assignment2 part 1-B

--- CS106A-Stanford/Assignment2/random_numbers.py
+++ CS106A-Stanford/Assignment2/random_numbers.py
@@ -10,12 +10,21 @@ import random
 
 def main():
     """
-    You should write your code for this program in this function.
-    Make sure to delete the 'pass' line before starting to write
-    your own code. You should also delete this comment and replace
-    it with a better, more descriptive one.
+    Part 1-B asks us to write a program that will generate and print 10 random integers to the terminal.
+    Constants are assigned for the amount of times Python will generate a random number, the minimum random integer,
+    and maximum random integer. Additionally, I added a num_label variable so that each random integer generated is
+    labeled in an orderly fashion.
+    A for loop is used to run the random.randint() function 10 times automatically and increments num_label
+    by 1 every time a single loop has completed.
     """
-    pass
+    NUM_RANDOM = 10
+    MIN_RANDOM = 0
+    MAX_RANDOM = 100
+    num_label = 1
+    print('Printing ten random integers...')
+    for i in range(NUM_RANDOM):
+        print(str(num_label) + '):' + ' ' + str(random.randint(MIN_RANDOM, MAX_RANDOM)))
+        num_label += 1
 
 
 # This provided line is required at the end of a Python file
