ba03b33ccaa7e6dc8ff5bd6d43f2292fa9fd74e6
Kevin Ihm <kevinihm@gmail.com>
2020-04-27 12:27:19 -0700

    completed hailstones partX

--- CS106A-Stanford/Assignment2/hailstones.py
+++ CS106A-Stanford/Assignment2/hailstones.py
@@ -13,7 +13,19 @@ def main():
     your own code. You should also delete this comment and replace
     it with a better, more descriptive one.
     """
-    pass
+    print('Hailstones...')
+    n = int(input('Please enter a positive integer: '))
+    count = 0
+    while n != 1:
+        if n % 2 == 0:
+            print(str(n) + ' is even, so half of it is: ' + str(n / 2))
+            n = n / 2
+            count += 1
+        else:
+            print(str(n) + ' is odd, so the value of 3n + 1 is: ' + str((n * 3) + 1))
+            n = (n * 3) + 1
+            count += 1
+    print('Finished... The process took ' + str(count) + ' steps to reach 1.')
 
 
 # This provided line is required at the end of a Python file
